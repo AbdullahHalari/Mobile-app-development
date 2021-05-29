653590465681c16bf249765a643ebab801d42134
AbdullahHalari <droidabdullah@gmail.com>
2021-05-29 20:24:07 +0500

    login_signup_form

---
+++ assignment2/loginpage/lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'Signup.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool _isHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('background1.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+                padding: EdgeInsets.all(40),
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      //  padding: EdgeInsets.all(10),
+                      height: 23.0,
+                      width: 290.0,
+                      padding: EdgeInsets.only(top: 40),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(200),
+                      ),
+                      child: Image.asset('logo.png'),
+                    ),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(fontSize: 20),
+                        )),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        style: TextStyle(color: Colors.blue),
+                        cursorColor: Colors.red,
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red)),
+                          hoverColor: Colors.red,
+                          labelStyle: TextStyle(color: Colors.grey),
+                          labelText: 'Username/Email',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        style: TextStyle(color: Colors.blue),
+                        cursorColor: Colors.red,
+                        controller: passwordController,
+                        obscureText: _isHidden,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          suffix: InkWell(
+                            onTap: _togglePasswordView,
+                            child: Icon(_isHidden
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red)),
+                          hoverColor: Colors.red,
+                          labelStyle: TextStyle(color: Colors.grey),
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        //forgot password screen
+                      },
+                      textColor: Colors.red,
+                      child: Text('Forgot Password'),
+                    ),
+                    Container(
+                        height: 50,
+                        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: RaisedButton(
+                          textColor: Colors.black,
+                          color: Colors.red,
+                          child: Text('Login'),
+                          onPressed: () {
+                            print(nameController.text);
+                            print(passwordController.text);
+                          },
+                        )),
+                    Container(
+                        child: Row(
+                      children: <Widget>[
+                        Text('Does not have account?'),
+                        FlatButton(
+                          textColor: Colors.blue,
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            //signup screen
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignupScreen()));
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+                  ],
+                ))));
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+}
